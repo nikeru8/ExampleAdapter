ded855e7f97cbaa741405b9ad4eefc1fa9dffcf2
nikeru8 <nikeru888@gmail.com>
2018-01-05 11:22:39 +0800

    version fix

--- app/src/main/java/com/hello/kaiser/exampleadapter/viewHolder/DogViewHolder.java
+++ app/src/main/java/com/hello/kaiser/exampleadapter/viewHolder/DogViewHolder.java
@@ -2,6 +2,8 @@ package com.hello.kaiser.exampleadapter.viewHolder;
 
 import android.support.annotation.LayoutRes;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.hello.kaiser.exampleadapter.R;
 import com.hello.kaiser.exampleadapter.model.Dog;
@@ -15,12 +17,27 @@ public class DogViewHolder extends AbstractViewHolder<Dog> {
     @LayoutRes
     public static final int LAYOUT = R.layout.layout_dog;
 
+    private Spinner mOne, mTwo;
+
     public DogViewHolder(View itemView) {
         super(itemView);
+        mOne = (Spinner) itemView.findViewById(R.id.one_spinner);
+        mTwo = (Spinner) itemView.findViewById(R.id.two_spinner);
     }
 
     @Override
     public void onBindViewHolder(Dog element) {
+        mOne.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //DoSomething
+                //to reWrite amount of mVisitables and change the words in TextView in Cat
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
